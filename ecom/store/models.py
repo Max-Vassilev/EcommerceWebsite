3025964cb61d1b53627b959569324d028ce0e266
Maxim <maximvassilev01@gmail.com>
2024-09-03 15:00:18 +0300

    Modified home page with actual products, added base.html and added about page.

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -20,6 +20,9 @@ class Product(models.Model):
     description = models.CharField(max_length=250, default="", blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product')
 
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
     def __str__(self):
         return self.name
 
