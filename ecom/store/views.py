cc7d59a7d31ca9aa3c2b153732b50142e78153fe
Maxim <maximvassilev01@gmail.com>
2024-09-06 16:11:36 +0300

    Added Login/Logout functionality

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
     products = Product.objects.all()
@@ -7,5 +9,26 @@ def home(request):
     return render(request, 'home.html', {'products': products})
 
 def about(request):
-
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+          
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
